345ede2fbfdd5a3f622e039bd74efc21e2e66578
victorgervasio <victorcarvalhogerv@gmail.com>
2026-03-07 01:32:31 -0300

    Makefile working properly.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
-export OPEN_BASEBAND_ROOT_DIR_PATH="$PWD"
-make -C "$OPEN_BASEBAND_ROOT_DIR_PATH/sdk"
-source ~/.bashrc
-make -C "$OPEN_BASEBAND_ROOT_DIR_PATH/sdk" LLVM_ENABLE_LLD=ON
-source ~/.bashrc
+make -C "${PWD}/sdk" LLVM_ENABLE_LLD=ON
+#source ~/.bashrc
+#make -C "${OPEN_BASEBAND_ROOT_DIR_PATH}/sdk" LLVM_ENABLE_LLD=ON
+#source ~/.bashrc
