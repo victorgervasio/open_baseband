a54aa50b39ca56695aec08d5066b5f38e9723d07
victorgervasio <victorcarvalhogerv@gmail.com>
2026-09-08 00:44:26 -0300

    refactor(change payload message): change payload message
    change payload message, from 'Hello NR LDPC!' to Starless lyrics, by KC. This could be interpreted
    as a feature, since now BER is more easy to visualize (since payload actually takes the most of the
    block size (around 80%), while before most errors were in padding region. Hope Fripp don't file a
    lawsuit against this dude

---
+++ openasip/targets/common/kernels/5gnr/ldpc/include/payload.h
@@ -0,0 +1,17 @@
+#include "defines.h"
+
+#ifndef PAYLOAD_H
+#define PAYLOAD_H
+const char message[MESSAGE_LENGTH] = "Sundown dazzling day "
+                                     "Gold through my eyes "
+                                     "But my eyes turned within only see "
+                                     "Starless and bible black "
+                                     "Old friend charity "
+                                     "Cruel twisted smile "
+                                     "And the smile signals emptiness for me "
+                                     "Starless and bible black "
+                                     "Ice blue silver sky "
+                                     "Fades into grey "
+                                     "To a grey hope that all yearns to be "
+                                     "Starless and bible black";
+#endif
